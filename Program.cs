724de8d7fe60e1586e93b5c2ba2702a03c4bcfee
OrkunDemir5 <orkun.d1999@gmail.com>
2024-11-04 13:14:09 +0300

    Pratik-Try-Catch

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿bool basarili = false;
+int sayi = 0;
+
+//her hata girdiğinde tekrara dönmesi için
+while (!basarili) 
+{
+    try
+    {
+        Console.WriteLine("Bir sayı giriniz");
+        sayi = Convert.ToInt32(Console.ReadLine());
+        basarili = true;
+
+    }// hatalı karakter girerse
+    catch (Exception ex)
+    {
+        Console.WriteLine("Lütfen sayı giriniz");
+    }
+    finally
+    {
+        // ne olursa olsun çalışacak kod
+    }
+}
+Console.WriteLine($"Başarılı girdiğiniz sayı : {sayi}");
+
+
